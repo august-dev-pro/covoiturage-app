536e189aad17cd8b60bd42e6476acb36d59ae9db
august-dev-pro <augustinselete01@gmail.com>
2024-02-12 12:56:53 +0000

    ...

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
@@ -7,24 +8,25 @@ const Header = () => {
         <div className="header_content content">
           <div className="logo-container">
             <div className="logo">IA-Covoitutage</div>
-            <nav className="nav">
-              <div className="nav_container">
-                <div className="nav_content">
-                  <div className="nav_links">
-                    <a href="/" className="link">
-                      <div className="content">home</div>
-                    </a>
-                    <a href="/" className="link">
-                      <div className="content">Nous Concernant</div>
-                    </a>
-                    <a href="/" className="link">
-                      <div className="content">home</div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </nav>
           </div>
+          <nav className="nav">
+            <div className="nav_container">
+              <div className="nav_content">
+                <div className="nav_links">
+                  <a href="/" className="link">
+                    <div className="content">Acceuil</div>
+                  </a>
+                  <a href="/" className="link">
+                    <div className="content">Nous Concernant</div>
+                  </a>
+                  <a href="/" className="link">
+                    <div className="content">s'inscrire</div>
+                  </a>
+                </div>
+                <button className="contact-btn">Nous contactez</button>
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
     </header>
