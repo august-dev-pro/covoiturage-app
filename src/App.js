543d0830ac2aa5dd8c524845391b90f32ceb95b5
august-dev-pro <augustinselete01@gmail.com>
2024-02-12 12:47:35 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./components/layout/header/Header";
 
 function App() {
   return (
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -7,6 +7,23 @@ const Header = () => {
         <div className="header_content content">
           <div className="logo-container">
             <div className="logo">IA-Covoitutage</div>
+            <nav className="nav">
+              <div className="nav_container">
+                <div className="nav_content">
+                  <div className="nav_links">
+                    <a href="/" className="link">
+                      <div className="content">home</div>
+                    </a>
+                    <a href="/" className="link">
+                      <div className="content">Nous Concernant</div>
+                    </a>
+                    <a href="/" className="link">
+                      <div className="content">home</div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
